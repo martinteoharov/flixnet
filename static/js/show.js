3ef447cedf75e3edd77c9d66e8737d9fc51e16f6
martinteoharov <martin.teoharov15@gmail.com>
2020-07-14 14:08:46 +0300

    client side youtube api p2 - rewrite so it doesn't use unnecessary libraries

--- static/js/show.js
+++ static/js/show.js
@@ -1,10 +1,12 @@
-import { test } from './youtube-api.js';
-$(document).ready(() => {
-	const title = document.getElementById('title').innerText;
-	const poster = document.getElementsByClassName('poster')[0];
-	fetchGetIMDB(title).then((res) => {
-		console.log(res);
-		poster.src = res.Poster;
+const title   = document.getElementById('title').innerText;
+const poster  = document.getElementsByClassName('poster')[0];
+const ytEmbed = document.getElementsByClassName('imdb-youtube-trailer')[0].childNodes[0];
+
+fetchGetIMDB(title).then((res) => {
+	console.log(res);
+	poster.src = res.Poster;
+	fetchGetYT({title: title + ' trailer'}).then((res) => {
+		ytEmbed.src = 'https://www.youtube.com/embed/' + res.items[0].id.videoId;
+
 	});
-	//handleClientLoad();
 });
--- static/js/youtube-api.js
+++ static/js/youtube-api.js
@@ -1,12 +1,14 @@
-const { google } = require('googleapis');
+const apiKey = 'AIzaSyCgejHyQE2eo_GaV-Bf7WjTUu46PgyxWqA';
+const url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCWnY0GoKFnFY0oeaEN605Ug&maxResults=50&key=' + apiKey;
 
-const youtubeAPI = google.data_v3({
-	version: 'v3',
-	auth: 'AIzaSyCxs3reS_X6ioh7nn8mwrqyhn6qRN96xGc' // specify your API key here
-});
-
-const test = async() => {
+const fetchGetYT = async({title}) => {
+	const url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + title + '&maxResults=1&key=' + apiKey;
+	const response = await fetch(url, {
+		method: "GET",
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		}
+	});
+	return await response.json(); // parses JSON response into native JavaScript objects
 }
-
-
-export  { test }
